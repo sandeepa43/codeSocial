d220ce44b30c3e9282295964d9688d1e002c2751
sandeepa <sandeepasingh686@gmail.com>
2020-05-04 15:30:14 +0530

    models and views

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port  =  8000;
-
+const expressLayouts = require('express-ejs-layouts');
 //use express router
-app.use('/',require('./routes'));
+app.use(expressLayouts);
+app.use('/', require('./routes'));
+
+//set up view engine 
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
