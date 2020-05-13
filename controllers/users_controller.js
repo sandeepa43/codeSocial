adda21bbef307a7508030bcabf063bc1cdcc7ca4
sandeepa <sandeepasingh686@gmail.com>
2020-05-13 11:08:10 +0530

    signin

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,18 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
+return res.render('user_profile', {
+    title:'User Profile'
+})
+}
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"codeSocial | SignUp"
+    });
+}
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sing_in',{
+        title:"codeSocial | signIn"
+    });
 }
--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port  =  8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+
+
 //use express router
+
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 app.use('/', require('./routes'));
 
 //set up view engine 
